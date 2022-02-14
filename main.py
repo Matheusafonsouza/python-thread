fc3b712968f51be5755e46e96e5a051104a7508f
matheusafonsouza <matheusafonsouza@gmail.com>
2022-02-14 02:13:13 -0300

    using threads for download files

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor
+import requests
 import time
 
 
-def delay(seconds: int) -> str:
+def download_file(url: str) -> None:
     """
-    Delay time, stoping code process for a given number of seconds.
-    :param seconds: Number of seconds to delay the application
+    Download a given file, using his url to do this.
+    :param url: File url to be downloaded
+    :returns: Success message
     """
-    time.sleep(seconds)
-    return f"{seconds} done..."
+    file_bytes = requests.get(url).content
+    file_name = url.split("/")[-1]
+    file_name = f"{file_name}.jpg"
+    with open(file_name, "wb") as file:
+        file.write(file_bytes)
+    print(f"{file_name} was downloaded...") 
 
 
 if __name__ == "__main__":
     start = time.perf_counter()
 
+    file_urls = [
+        'https://images.unsplash.com/photo-1516117172878-fd2c41f4a759',
+        'https://images.unsplash.com/photo-1532009324734-20a7a5813719',
+        'https://images.unsplash.com/photo-1524429656589-6633a470097c',
+        'https://images.unsplash.com/photo-1530224264768-7ff8c1789d79',
+        'https://images.unsplash.com/photo-1564135624576-c5c88640f235',
+        'https://images.unsplash.com/photo-1541698444083-023c97d3f4b6',
+        'https://images.unsplash.com/photo-1522364723953-452d3431c267',
+        'https://images.unsplash.com/photo-1513938709626-033611b8cc03',
+        'https://images.unsplash.com/photo-1507143550189-fed454f93097',
+        'https://images.unsplash.com/photo-1493976040374-85c8e12f0c0e',
+        'https://images.unsplash.com/photo-1504198453319-5ce911bafcde',
+        'https://images.unsplash.com/photo-1530122037265-a5f1f91d3b99',
+        'https://images.unsplash.com/photo-1516972810927-80185027ca84',
+        'https://images.unsplash.com/photo-1550439062-609e1531270e',
+        'https://images.unsplash.com/photo-1549692520-acc6669e2f0c'
+    ]
+
     with ThreadPoolExecutor() as executor:
-        seconds = [5, 4, 3, 2, 1]
-        results = executor.map(delay, seconds)
-        for result in results:
-            print(result)
+        executor.map(download_file, file_urls)
 
     finish = time.perf_counter()
     print(f"Finished in {round(finish-start, 2)} seconds")
