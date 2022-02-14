3f36156c68b4081ccf47e286ad716fe514abcd4d
matheusafonsouza <matheusafonsouza@gmail.com>
2022-02-14 02:01:05 -0300

    using threadpool for threads

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
-from threading import Thread
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
 
 
-def delay(seconds: int) -> None:
+def delay(seconds: int) -> str:
     """
     Delay time, stoping code process for a given number of seconds.
     :param seconds: Number of seconds to delay the application
     """
     time.sleep(seconds)
+    return f"{seconds} done..."
 
 
 if __name__ == "__main__":
     start = time.perf_counter()
 
-    threads = list()
-    for _ in range(10):
-        t = Thread(target=delay, args=[2])
-        t.start()
-        threads.append(t)
-
-    for thread in threads:
-        thread.join()
+    with ThreadPoolExecutor() as executor:
+        seconds = [5, 4, 3, 2, 1]
+        results = executor.map(delay, seconds)
+        for result in results:
+            print(result)
 
     finish = time.perf_counter()
     print(f"Finished in {round(finish-start, 2)} seconds")
